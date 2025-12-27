6ad0428f36e3d454f36ca1c9f87a56c464270b95
Keertika Anand <keertika1612@gmail.com>
2025-12-27 19:38:08 +0530

    Time: 8 ms (9.90%) | Memory: 16.5 MB (48.38%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -3,11 +3,10 @@ public:
     int minPartitions(string n) {
         int greatest=0;
         for(int i=0;i<n.size();i++){
-            int x=n[i]-'0';
-            if(x>greatest){
-                greatest=x;
+            if(n[i]>greatest){
+                greatest=n[i];
             }
         }
-        return greatest;
+        return greatest-'0';
     }
 };
